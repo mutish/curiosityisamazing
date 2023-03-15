1236cd44935dadd662e2c531cab58a0d7722d08e
mutish <mutindastacey@gmail.com>
2023-03-15 19:29:26 +0300

    infinity

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle import *
+speed(10)
+color('yellow')
+bgcolor('black')
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
+
+speed(10)
+color('purple')
+bgcolor('black')
+a = 200
+while a > 0:
+    left(a)
+    forward(a * 3)
+    a = a - 1
